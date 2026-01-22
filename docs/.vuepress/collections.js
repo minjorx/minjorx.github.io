0935ce93f9d06859ee7ea4781551acae48e33a0a
minjor <minjor@minjor.com>
2026-01-22 20:36:53 +0800

    增加同步功能3

--- docs/.vuepress/collections.js
+++ docs/.vuepress/collections.js
@@ -24,17 +24,17 @@
  *
  * 通过 `defineCollection` 定义的 collection 配置，应该填入 `defineCollections` 中
  */
-import { defineCollection, defineCollections } from 'vuepress-theme-plume'
+import { defineCollection, defineCollections } from "vuepress-theme-plume";
 
 const blog = defineCollection({
   // post 类型，这里用于实现 博客功能
-  type: 'post',
+  type: "post",
   // 文档集合所在目录，相对于 `docs`
-  dir: 'blog',
+  dir: "blog",
   // 文档标题，它将用于在页面的面包屑导航中显示
-  title: 'Blog',
+  title: "Blog",
   // 文章列表页的链接，如果 `linkPrefix` 未定义，它也将作为 相关的文章的 permalink 的前缀
-  link: '/blog/',
+  link: "/blog/",
   // linkPrefix: '/article/', // 相关文章的链接前缀
   postList: true, // 是否启用文章列表页
   tags: true, // 是否启用标签页
@@ -43,30 +43,28 @@ const blog = defineCollection({
   // postCover: 'right', // 文章封面位置
   pagination: 15, // 每页显示文章数量
   // sidebar: 'auto',
-})
+});
 
 const demoDoc = defineCollection({
   // doc 类型，该类型带有侧边栏
-  type: 'doc',
+  type: "doc",
   // 文档集合所在目录，相对于 `docs`
-  dir: 'demo',
+  dir: "demo",
   // `dir` 所指向的目录中的所有 markdown 文件，其 permalink 需要以 `linkPrefix` 配置作为前缀
   // 如果 前缀不一致，则无法生成侧边栏。
   // 所以请确保  markdown 文件的 permalink 都以 `linkPrefix` 开头
-  linkPrefix: '/demo',
+  linkPrefix: "/demo",
   // 文档标题，它将用于在页面的面包屑导航中显示
-  title: 'Demo',
+  title: "Demo",
   // 手动配置侧边栏结构
   // sidebar: ['', 'foo', 'bar'],
   // 根据文件结构自动生成侧边栏
   // sidebar: 'auto',
-})
+});
 
 /**
  * 导出所有的 collections
  * (blog 为博客示例，如果不需要博客功能，请删除)
  * (demoDoc 为参考示例，如果不需要它，请删除)
  */
-export default defineCollections([
-  blog,
-])
+export default defineCollections([blog]);
